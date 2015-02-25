03995c02440cdb00f39ccfc921d38550008414bf
unknown <Bswift@BSWIFT3.gro.Doncasters.com>
2015-02-25 14:21:36 -0500

    Only edited server/client messages

--- SaveDataProtocol.py
+++ SaveDataProtocol.py
@@ -1,7 +1,14 @@
 
-client_msg = {
-	"RECV_PARTSTRANS":1,
-	"SEND_PARTSTRANS":2,
-	"RECV_SAVEAS":3,
-	"SEND_SAVEAS", 4
+# Messages that will only be sent by client
+client_msgs = {
+	"GIVE_PARTSTRANS":1,
+	"GIVING_PARTSTRANS":2,
+	"GIVE_SAVEAS":3,
+	"GIVING_SAVEAS", 4
+}
+
+# Messages that will only be sent by server
+server_msgs = {
+	"GIVING_PARTSTRANS":1,
+	"GIVING_SAVEAS":2,
 }
