bc15726b91d07e9da84322792c5b372313b455d8
dserver <jonathanhenk@gmail.com>
2015-02-25 18:44:29 -0800

    Making awesome progress in pywin32. Need to figure out how to return FALSE

--- WinSendMessage.py
+++ WinSendMessage.py
@@ -1,18 +1,63 @@
-import win32api, win32con, win32gui, win32ui, win32service, os, time
+import win32gui, win32process
+import sys
+import re
 
 class Window:
     def __init__(self):
         self._hwnd = None
         self._text = None
 
+
+# Globals
+found = False
+_hwnd = None
+_child_hwnd = None
+
+
 def enum_window_callback(hwnd, extra):
-    print win32gui.GetWindowText(hwnd)
+    find_title = re.compile(re.escape(extra[0]))
+    wintitle = win32gui.GetWindowText(hwnd)
+    #print wintitle
+    matches = re.findall(find_title, wintitle)
+    if len(matches)>0:
+        global found
+        global _hwnd
+        found = True
+        _hwnd = hwnd
+        
+def enum_child_callback(hwnd, extra):
+    find_title = re.compile(re.escape(extra[0]))
+    wintitle = win32gui.GetWindowText(hwnd)
+    print wintitle
+    matches = re.findall(find_title, wintitle)
+    if len(matches)>0:
+        global found
+        global _child_hwnd
+        found = True
+        _child_hwnd = hwnd
+        return False # needed to stop enumerating through windows
+    else:
+        found = False
 
-def enum_windows():
-    win32gui.EnumWindows(enum_window_callback, None)
+# for each window handle found, enum_window_callback is called
+def find_window(title):
+    win32gui.EnumWindows(enum_window_callback, (title,))
 
-def get_window_by_text(wintext):
-    pass
+def find_child_window(threadId, child_title):
+    win32gui.EnumThreadWindows(threadId, enum_child_callback, (child_title,))
 
-enum_windows()
+find_window("Notepad++")
+if not found:
+    sys.exit(0)
 
+
+(threadId, processId) = win32process.GetWindowThreadProcessId(_hwnd)
+
+find_child_window(threadId, "Preferences")
+
+if not found:
+    print "Couldn't find child window"
+    sys.exit(0)
+
+print "Found preferences window"
+print _child_hwnd
